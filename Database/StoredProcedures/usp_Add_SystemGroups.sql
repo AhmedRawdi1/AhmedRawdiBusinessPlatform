3db727b216ff9b8609b7417410392e23dd7702f6
AhmedRawdi1 <ahmedrawdi1@users.noreply.github.com>
2026-08-06 14:01:29 +0300

    Modify stored procedure usp_Add_SystemGroups to incorporate Group ID into the Code column

--- Database/StoredProcedures/usp_Add_SystemGroups.sql
+++ Database/StoredProcedures/usp_Add_SystemGroups.sql
@@ -2,7 +2,7 @@ USE [ARBP];
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[usp_Add_SystemGroups]
-    @Code nvarchar(20),
+    @Code nvarchar(50),
     @EngName nvarchar(150),
     @ArbName nvarchar(150),
     @IsActive bit,
@@ -33,40 +33,56 @@ BEGIN
             THROW 50002, 'Group English name is required.', 1;
 
         IF @ArbName IS NULL
-            THROW 50003, 'Group Arabic name is required.', 1;
+            SET @ArbName = @EngName;
 
         BEGIN TRANSACTION;
 
         IF ISNULL(@GroupID, 0) = 0
         BEGIN
-            IF EXISTS (SELECT 1 FROM dbo.SystemGroups WITH (UPDLOCK, HOLDLOCK) WHERE Code = @Code)
+            DECLARE @NextID BIGINT = ISNULL((SELECT MAX(ID) FROM dbo.SystemGroups WITH (UPDLOCK, HOLDLOCK)), 0) + 1;
+            DECLARE @IdStr NVARCHAR(20) = CAST(@NextID AS NVARCHAR(20));
+            DECLARE @FinalCode NVARCHAR(20) = @Code;
+
+            IF @Code NOT LIKE '%' + @IdStr
+            BEGIN
+                SET @FinalCode = SUBSTRING(@Code + '_' + @IdStr, 1, 20);
+            END;
+
+            IF EXISTS (SELECT 1 FROM dbo.SystemGroups WITH (UPDLOCK, HOLDLOCK) WHERE Code = @FinalCode)
                 THROW 50004, 'A system group with the same code already exists.', 1;
 
             INSERT dbo.SystemGroups
-                (Code, EngName, ArbName, IsActive, RegBy, ExpiredDate)
+                (ID, Code, EngName, ArbName, IsActive, RegBy, ExpiredDate, RegDate)
             VALUES
-                (@Code, @EngName, @ArbName, @IsActive, @RegBy, @ExpiredDate);
+                (@NextID, @FinalCode, @EngName, @ArbName, @IsActive, @RegBy, @ExpiredDate, GETDATE());
 
-            SET @NewGroupID = CONVERT(bigint, SCOPE_IDENTITY());
+            SET @NewGroupID = @NextID;
         END
         ELSE
         BEGIN
             IF NOT EXISTS (SELECT 1 FROM dbo.SystemGroups WITH (UPDLOCK, HOLDLOCK) WHERE ID = @GroupID)
                 THROW 50005, 'The system group to update was not found.', 1;
 
-            IF EXISTS (SELECT 1 FROM dbo.SystemGroups WITH (UPDLOCK, HOLDLOCK) WHERE Code = @Code AND ID <> @GroupID)
+            SET @NewGroupID = @GroupID;
+            DECLARE @UpdateIdStr NVARCHAR(20) = CAST(@GroupID AS NVARCHAR(20));
+            DECLARE @UpdatedCode NVARCHAR(20) = @Code;
+
+            IF @Code NOT LIKE '%' + @UpdateIdStr
+            BEGIN
+                SET @UpdatedCode = SUBSTRING(@Code + '_' + @UpdateIdStr, 1, 20);
+            END;
+
+            IF EXISTS (SELECT 1 FROM dbo.SystemGroups WITH (UPDLOCK, HOLDLOCK) WHERE Code = @UpdatedCode AND ID <> @GroupID)
                 THROW 50004, 'A system group with the same code already exists.', 1;
 
             UPDATE dbo.SystemGroups
-            SET Code = @Code,
+            SET Code = @UpdatedCode,
                 EngName = @EngName,
                 ArbName = @ArbName,
                 IsActive = @IsActive,
                 RegBy = COALESCE(@RegBy, RegBy),
                 ExpiredDate = @ExpiredDate
             WHERE ID = @GroupID;
-
-            SET @NewGroupID = @GroupID;
         END;
 
         COMMIT TRANSACTION;
@@ -78,7 +94,6 @@ BEGIN
         SET @HasError = 1;
         SET @ErrorDesc = ERROR_MESSAGE();
 
-        -- Preserve the existing procedure behavior for callers that handle SQL exceptions.
         THROW;
     END CATCH;
 END;
